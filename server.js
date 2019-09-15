16863dbe88389d7a8e33e29df561261f23686e58
Carl Medina <carl.l.medina@comcast.net>
2019-09-14 20:34:56 -0700

    make it better

--- server.js
+++ server.js
@@ -2,15 +2,21 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var apiRoutes = require("./app/routing/apiRoutes")
+var htmlRoutes = require("./app/routing/htmlRoutes")
 
-// Sets up the Express App
-// =============================================================
+
 var app = express();
 var PORT = 3000;
+
+var PORT = process.env.PORT || 8080
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
 
 
 // Starts the server to begin listening
