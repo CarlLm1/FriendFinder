5b088023a912a7016d555e24493bb5ee6a445031
Carl Medina <carl.l.medina@comcast.net>
2019-09-14 18:43:27 -0700

    added GET and POST routes to htmlRoutes and apiRoutes, displaying the survey page, home page, and handling incoming survery results

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,18 @@
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
+
+var express = require("express")
+var router = express.Router()
+var path = require("path")
+
+  router.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+
+  router.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+  })
+
+  router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+
+  module.exports = router
